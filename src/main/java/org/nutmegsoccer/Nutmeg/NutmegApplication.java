47c3fa3f2923345889fbeda3039c454d0c7b226a
Felix Pan <felixpan238@gmail.com>
2017-09-02 22:09:02 -0400

    Initial commit

---
+++ src/main/java/org/nutmegsoccer/Nutmeg/NutmegApplication.java
@@ -0,0 +1,12 @@
+package org.nutmegsoccer.Nutmeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutmegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutmegApplication.class, args);
+	}
+}
---
+++ src/test/java/org/nutmegsoccer/Nutmeg/NutmegApplicationTests.java
@@ -0,0 +1,16 @@
+package org.nutmegsoccer.Nutmeg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NutmegApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
