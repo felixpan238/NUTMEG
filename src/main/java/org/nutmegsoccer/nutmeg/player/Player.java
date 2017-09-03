87efc110e5d314df9154ad8652264e5ef93f4f44
Julian Martins <blind.apex@gmail.com>
2017-09-02 23:49:22 -0400

    Player Update

--- src/main/java/org/nutmegsoccer/nutmeg/player/Player.java
+++ src/main/java/org/nutmegsoccer/nutmeg/player/Player.java
@@ -3,26 +3,47 @@ package org.nutmegsoccer.nutmeg.player;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.nutmegsoccer.nutmeg.address.Address;
+import org.nutmegsoccer.nutmeg.phone.Phone;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.sql.Date;
+import java.util.List;
 
 @Entity
 @Table(name="player")
 public class Player {
-    @Id @Column(name = "id", nullable = false)
+    
+	@Id 
+    @Column(name = "id", nullable = false)
     private int id;
-    @Column(name = "first_name", nullable = false)
+    
+	@Column(name = "first_name", nullable = false)
     private String firstName;
-    @Column(name = "last_name", nullable = false)
+    
+	@Column(name = "last_name", nullable = false)
     private String lastName;
-    @Column(name = "gender", nullable = false)
+    
+	@Column(name = "gender", nullable = false)
     private String gender;
-    @Column(name = "birthday", nullable = false)
+    
+	@Column(name = "birthday", nullable = false)
     private Date birthday;
-    @Column(name = "address_id", nullable = false)
-    private int addressId;
-    @Column(name = "phone_id", nullable = false)
-    private int phoneId;
+    
+	@OneToOne
+	@Column(name = "address_id", referencedColumnName="id", nullable = false)
+	private Address address;
+	
+	@ManyToOne
+	@JsonProperty(access = JsonProperty.Access.READ_ONLY)
+	@Column(name = "phone_id", referencedColumnName="id", nullable = false)
+	private List<Phone> phone;
+	
 
     public Player(){
 
@@ -68,19 +89,19 @@ public class Player {
         this.birthday = birthday;
     }
 
-    public int getAddressId() {
-        return addressId;
+    public Address getAddress() {
+        return address;
     }
 
-    public void setAddressId(int addressId) {
-        this.addressId = addressId;
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
+    public List<Phone> getPhone() {
+        return phone;
     }
 
-    public int getPhoneId() {
-        return phoneId;
-    }
-
-    public void setPhoneId(int phoneId) {
-        this.phoneId = phoneId;
+    public void setPhone(List<Phone> phone) {
+        this.phone = phone;
     }
 }
